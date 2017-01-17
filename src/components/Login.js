b991c0e7a1ed8b3094c58f14865c85ed9a1914a2
Margaux <154373567@qq.com>
2017-01-17 15:44:51 +0800

    add login component and style

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import { FormGroup, InputGroup, FormControl, Button } from 'react-bootstrap';
+import { bootstrapUtils } from 'react-bootstrap/lib/utils';
+import './Login.css';
+
+bootstrapUtils.addStyle(InputGroup.Addon, 'custom-user');
+bootstrapUtils.addStyle(InputGroup.Addon, 'custom-password');
+bootstrapUtils.addStyle(InputGroup, 'custom');
 
 class Login extends React.Component{
 
+  render(){
+    return(
+      <div className="login">
+        <h3>登录</h3>
+        <form>
+          <FormGroup>
+            <InputGroup bsStyle="custom">
+              <InputGroup.Addon bsStyle="custom-user"></InputGroup.Addon>
+              <FormControl type="text" />
+            </InputGroup>
+          </FormGroup>
+          <FormGroup>
+            <InputGroup bsStyle="custom">
+              <InputGroup.Addon bsStyle="custom-password"></InputGroup.Addon>
+              <FormControl type="text" />
+            </InputGroup>
+          </FormGroup>
+        </form>
+        <Button bsStyle="danger">登录</Button>
+      </div>
+    );
+  }
 }
 
 export default Login;
