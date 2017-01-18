2a6bd9b25cbec51d2faa1bf5a6616b71553525cf
Margaux <154373567@qq.com>
2017-01-18 22:51:48 +0800

    add sourcelist component style

--- src/components/SourceShareList.js
+++ src/components/SourceShareList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ajax from 'superagent';
 import './SourceShareList.css';
+import { Button, NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 class SourceShareList extends React.Component{
   constructor(props){
@@ -29,24 +31,37 @@ class SourceShareList extends React.Component{
       <div className="source-share-list">
         <h1 className="title-h">资源分享</h1>
         <p className="title-p"><label className="of"></label>来自IT各个领域的学习资源<label className="on"></label></p>
+        <div className="source-card-list">
         {
           this.state.sourceList.map((source) => {
             const id = source.id;
             const userName = source.username;
             const urlMessage = source.urlmessage;
             const urlintroduce = source.urlintroduce;
-            const urlPubulishTime = source.urlpublish_time;
+            const urlPubulishTime = source.urlpublish_time.slice(0, 16);
             const urlReadCount = source.urlreadcount;
 
             return (
-              <div key={source.id}>
-                <p>BY{userName}{urlPubulishTime}</p>
-                <p>{urlintroduce}</p>
-                <div>了解详情</div>
-              </div>
+              
+                <div className="source-card" key={source.id}>
+                  <p>
+                    <b className="b-by">BY</b>
+                    <b className="b-username">{userName}</b>
+                    <b className="b-publishtime">{urlPubulishTime}</b>
+                    阅读量：{urlReadCount}
+                  </p>
+                  <p>{urlintroduce}</p>
+                  <LinkContainer to="/source-share">
+                    <NavItem>                           {/* 防止<Button> 被自动转换成<a>导致样式混乱 */}
+                      <Button bsStyle="danger">了解详情</Button>
+                    </NavItem>
+                  </LinkContainer>
+                </div>
+              
             )
-          })
-        }
+          }
+          )
+        }</div>
       </div>
     )
   }
--- src/components/WriteSource.js
+++ src/components/WriteSource.js
@@ -16,6 +16,26 @@ class WriteSource extends React.Component{
     ajax.post("")
   }
 
+  publish(){
+    // const publishContent = {
+    //   urlMessage: this.refs.url.value,
+    //   urlIntroduce: this.refs.introduce.value
+    // }
+
+    // const baseUrl="http://182.92.209.117:2168/fundpart";
+    // ajax.post(`${baseUrl}/urlpublish`)
+    // .send(publishContent)
+    // .set('Accept', 'application/json')
+    // .end(function(error, response){
+    //  if (error || !response.ok) {
+    //    alert('Oh no! error');
+    //  } else {
+    //    alert('yay got ' + JSON.stringify(response.body));
+    //  }
+    // });
+    alert(this.refs.url.value);
+  }
+
   render(){
     return(
       <div key="write-source" className="write-source">
@@ -23,13 +43,13 @@ class WriteSource extends React.Component{
         <p className="title-p"><label className="of"></label>在这里添加链接<label className="on"></label></p>
         <form>
           <FormGroup bsSize="large">
-            <FormControl type="text" placeholder="添加链接" />
+            <FormControl type="text" placeholder="添加链接" inputRef={ url => { this.input = url; }} />
           </FormGroup>
           <FormGroup controlId="formControlsTextarea" bsStyle="custom">
-            <FormControl componentClass="textarea" placeholder="请输入关于链接的描述.." />
+            <FormControl componentClass="textarea" placeholder="请输入关于链接的描述.." inputRef={ introduce => { this.input = introduce; }} />
           </FormGroup>
         </form>
-        <Button bsStyle="danger" bsSize="large">提交</Button>
+        <Button bsStyle="danger" bsSize="large" onClick={this.publish.bind(this)}>提交</Button>
       </div>
     )
   }
